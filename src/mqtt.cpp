ea4c3da5fdfa5580885b19044c0583ec749e3941
Sylvain Lamboley <sylvainlamboley@mlt-automotive.fr>
2023-08-20 00:10:39 +0200

    payload en char*

--- src/mqtt.cpp
+++ src/mqtt.cpp
@@ -28,11 +28,20 @@ void ac_send() {
 #endif
 }
 
+void msg_from_payload(char* zone, uint8_t* payload, unsigned int payload_size) {
+    memcpy(zone, payload, payload_size);
+    zone[payload_size] = '\0';
+}
+
 void callback(char* topic, uint8_t* payload, unsigned int payload_size) {
     Serial.print("callback : Message arrived [");
     Serial.print(topic);
     Serial.println("] ");
-    if (strcmp(HA_request_AC_on, topic) == 0) {  // ON // TODO : remplacer par strcmp
+
+    char msg[120];
+    msg_from_payload(msg, payload, payload_size);
+
+    if (strcmp(HA_request_AC_on, topic) == 0) {  // ON
         Serial.println("HA request AC on");
         ac.on();
         ac_send();
@@ -41,14 +50,7 @@ void callback(char* topic, uint8_t* payload, unsigned int payload_size) {
         ac.off();
         ac_send();
     } else if (strcmp(HA_request_temperature_value, topic) == 0) {  // temperature
-        char payloadStr[payload_size + 1];
-        for (size_t i = 0; i < payload_size; ++i) {
-            payloadStr[i] = payload[i];
-        }
-        payloadStr[payload_size] = '\0';
-
-        int target_temperature = atoi(payloadStr);
-
+        int target_temperature = atoi(msg);
         if (target_temperature <= 30 && target_temperature >= 17) {
             Serial.print("Temperature request : ");
             Serial.println(target_temperature);
@@ -57,40 +59,34 @@ void callback(char* topic, uint8_t* payload, unsigned int payload_size) {
         }
 
     } else if (strcmp(HA_request_a_c_mode, topic) == 0) {  // mode
-        if (memcmp("Auto", payload, payload_size) == 0) {
+        if (strcmp("Auto", msg) == 0) {
             Serial.println("A/C mode request : auto");
             ac.setMode(kToshibaAcAuto);
             ac_send();
-        } else if (memcmp("Cool", payload, payload_size) == 0) {
+        } else if (strcmp("Cool", msg) == 0) {
             Serial.println("A/C mode request : cool");
             ac.setMode(kToshibaAcCool);
             ac_send();
-        } else if (memcmp("Dry", payload, payload_size) == 0) {
+        } else if (strcmp("Dry", msg) == 0) {
             Serial.println("A/C mode request : dry");
             ac.setMode(kToshibaAcDry);
             ac_send();
-        } else if (memcmp("Heat", payload, payload_size) == 0) {
+        } else if (strcmp("Heat", msg) == 0) {
             Serial.println("A/C mode request : heat");
             ac.setMode(kToshibaAcHeat);
             ac_send();
-        } else if (memcmp("Fan", payload, payload_size) == 0) {
+        } else if (strcmp("Fan", msg) == 0) {
             Serial.println("A/C mode request : fan");
             ac.setMode(kToshibaAcFan);
             ac_send();
         }
     } else if (strcmp(HA_request_a_c_fan_speed, topic) == 0) {  // fan speed
-        if (memcmp("Auto", payload, payload_size) == 0) {
+        if (strcmp("Auto", msg) == 0) {
             Serial.println("Fan speed request : auto");
             ac.setFan(kToshibaAcFanAuto);
             ac_send();
         } else {
-            char payloadStr[payload_size];
-            for (size_t i = 0; i < payload_size; ++i) {
-                payloadStr[i] = static_cast<char>(payload[i]);
-            }
-            payloadStr[payload_size - 1] = '\0';
-
-            int fan_speed = atoi(payloadStr);
+            int fan_speed = atoi(msg);
 
             if (fan_speed <= 5 && fan_speed >= 0) {
                 Serial.print("Fan speed request : ");
@@ -100,11 +96,11 @@ void callback(char* topic, uint8_t* payload, unsigned int payload_size) {
             }
         }
     } else if (strcmp(HA_request_a_c_fan_swing, topic) == 0) {  // swing
-        if (memcmp("On", payload, payload_size) == 0) {
+        if (strcmp("On", msg) == 0) {
             Serial.println("Fan swing request : on");
             ac.setSwing(kToshibaAcSwingOn);
             ac_send();
-        } else if (memcmp("Off", payload, payload_size) == 0) {
+        } else if (strcmp("Off", msg) == 0) {
             Serial.println("Fan swing request : off");
             ac.setSwing(kToshibaAcSwingOff);
             ac_send();
